be298bb432f02106accff90edcb47b40c17f1f3c
Jylhis <markus@jylhis.com>
2016-04-26 18:08:47 +0300

    Score class

---
+++ Game/Score.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Copyright 2016 Markus Jylhänkangas, Pauli Kokkonen & Veeti Karttunen
+ *
+ * Tämä tiedosto on osa Olio- ja käyttöliittymien ohjelmointi kurssin harjoitustyötä.
+ *
+ * Created: 26.04.2016
+ */
+
+using System;
+
+namespace src
+{
+    public class Score
+    {
+        private int currentScore;
+
+        public Score()
+        {
+        }
+
+    }
+}
+
